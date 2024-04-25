1716a7aa89363095c5052a23c10b98da9e834052
arthur <arthurdesouzareis222@gmail.com>
2024-04-25 15:02:23 -0300

    chore: criar lista de produtos

---
+++ src/data.ts
@@ -0,0 +1,62 @@
+export const ProductsItems = [
+  {
+    name: 'Jogo God of War: Ragnarok - PS4',
+    price: 238,
+    banner:
+      'https://cdn.awsli.com.br/2500x2500/241/241991/produto/122597245/7796c502a4.jpg',
+  },
+  {
+    name: 'Jogo MediEvil - PS4',
+    price: 53,
+    banner:
+      'https://cdn.awsli.com.br/2500x2500/241/241991/produto/43368261a6db7da857.jpg',
+  },
+  {
+    name: 'Jogo Grand Theft Auto V (Premium Edition) - PS4',
+    price: 74,
+    banner:
+      'https://cdn.awsli.com.br/2500x2500/241/241991/produto/28459116/6a10ea3ea6.jpg',
+  },
+  {
+    name: 'Jogo Fallout 76 - PS4',
+    price: 42,
+    banner:
+      'https://cdn.awsli.com.br/2500x2500/241/241991/produto/30177964/92b13218a2.jpg',
+  },
+  {
+    name: 'Jogo Need for Speed Rivals - Xbox One',
+    price: 71,
+    banner:
+      'https://cdn.awsli.com.br/2500x2500/241/241991/produto/2651595091161272204.jpg',
+  },
+  {
+    name: 'Jogo Tekken 8 - PS5',
+    price: 275,
+    banner:
+      'https://cdn.awsli.com.br/2500x2500/241/241991/produto/248945602/shp5-092-qk6pjbs0mx.png',
+  },
+  {
+    name: 'Jogo The Last Of Us Part II: Remastered - PS5',
+    price: 190,
+    banner:
+      'https://cdn.awsli.com.br/2500x2500/241/241991/produto/245514040/tlou-23k82hvvr8.jpg',
+  },
+  {
+    name: 'Jogo Darksiders III - Xbox One',
+    price: 71,
+    banner:
+      'https://cdn.awsli.com.br/2500x2500/241/241991/produto/18463648/6e92352019.jpg',
+  },
+  {
+    name: 'Jogo NBA 2K22 - Xbox Series X',
+    price: 67,
+    banner:
+      'https://cdn.awsli.com.br/2500x2500/241/241991/produto/117280217/756439e01f.jpg',
+  },
+  {
+    name: 'Jogo Marvels Spider-Man: Miles Morales - PS5',
+    price: 245,
+    banner:
+      'https://cdn.awsli.com.br/2500x2500/241/241991/produto/112978201/ce84f58578.jpg',
+  },
+];
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
 const Home = () => {
-  return <section className="container">Products</section>;
+  return <section className="container grid grid-cols-4 gap-4">
+Products
+  </section>;
 };
 
 export default Home;
